c25b82835788eccc028469fe0176ad0fdc4bf0c2
Anoencs <57495997+Anoencs@users.noreply.github.com>
2022-07-03 22:53:40 +0700

    Delete TypeScript directory

--- TypeScript/test.js
+++
@@ -1,4 +0,0 @@
-var x = "global";
-var y = "global";
-console.log(this.x); // "global"
-console.log(this.y); // undefined
--- TypeScript/test.ts
+++
@@ -1,4 +0,0 @@
-var x = "global";
-let y = "global";
-console.log(this.x); // "global"
-console.log(this.y); // undefined
